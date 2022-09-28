3bbe1d34a842eea8ba21911fe5ffc1651e774103
Hujaifa21 <21hujaifa@gmail.com>
2022-09-28 18:18:26 +0600

    making admin authorized

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import { useQuery } from 'react-query';
 import Loading from '../shared/Loading';
+import UserRow from './UserRow';
 
 const Users = () => {
-  const { data: users, isLoading } = useQuery('users', () => fetch('http://localhost:5000/users').then(res => res.json()));
+  const { data: users, isLoading, refetch } = useQuery('users', () => fetch('http://localhost:5000/users', {
+    method: 'GET',
+    headers: {
+      authorization: `Bearer ${localStorage.getItem('accessToken')}`
+    }
+  }).then(res => res.json()));
 
   if (isLoading) {
     return <Loading />
@@ -16,18 +22,13 @@ const Users = () => {
           <thead>
             <tr>
               <th></th>
-              <th>Name</th>
+              <th>Email</th>
               <th>Job</th>
               <th>Favorite Color</th>
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <th>1</th>
-              <td>Cy Ganderton</td>
-              <td>Quality Control Specialist</td>
-              <td>Blue</td>
-            </tr>
+            {users?.map(user => <UserRow key={user._id} user={user} refetch={refetch} />)}
           </tbody>
         </table>
       </div>
